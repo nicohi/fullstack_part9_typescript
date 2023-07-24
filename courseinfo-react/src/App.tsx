7c487e0d9b3b935f5ef67ce5f8751d31e677f7e9
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-07-24 21:28:22 +0300

    Improve

--- courseinfo-react/src/App.tsx
+++ courseinfo-react/src/App.tsx
@@ -1,10 +1,11 @@
 import Header from './components/Header';
 import Content from './components/Content';
 import Total from './components/Total';
+import { CoursePart } from './types';
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[]  = [
     {
       name: "Fundamentals",
       exerciseCount: 10
