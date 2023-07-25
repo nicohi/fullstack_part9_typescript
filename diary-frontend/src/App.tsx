94c0f923adc4befadef2e71a745eba1ee95f9e44
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-07-25 10:48:56 +0300

    Complete 9.18

--- diary-frontend/src/App.tsx
+++ diary-frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
+
 import { NewDiaryEntry, NonSensitiveDiaryEntry, Weather, Visibility } from "./types";
 import diaryService from './services/diaries';
+import Notification from './components/Notification';
 
 const initialDiary:NewDiaryEntry = {
   date: '',
@@ -13,23 +16,41 @@ const App = () => {
   const [diaries, setDiaries] = useState<NonSensitiveDiaryEntry[]>([]);
   const [newDiary, setNewDiary] = useState<NewDiaryEntry>(initialDiary);
 
+  const [message, setMessage] = useState<string>('');
+
+  const notify = (m:string) => {
+    setMessage(m);
+    setTimeout(() => setMessage(''), 3000);
+  };
+
   useEffect(() => {
     diaryService.getAll().then(data => {
       setDiaries(data);
     });
   }, []);
 
-  const diaryCreation = (event: React.SyntheticEvent) => {
+  const diaryCreation = async (event: React.SyntheticEvent) => {
     event.preventDefault();
-    diaryService.create(newDiary).then(data => {
-      setDiaries(diaries.concat(data));
-    });
+    try {
+      const data = await diaryService.create(newDiary);
+      await setDiaries(diaries.concat(data));
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log(error.status)
+        console.error(error.response);
+        if (error.response && error.response.data)
+          notify(error.response.data);
+      } else {
+        console.error(error);
+      }
+    }
     setNewDiary(initialDiary);
   };
 
   return (
     <div>
       <h1>Add new entry</h1>
+      <Notification message={message} />
       <form onSubmit={diaryCreation}>
         <div>date:<input value={newDiary.date} onChange={(event) => setNewDiary({ ...newDiary, date: event.target.value})} /></div>
         <div>weather:<input value={newDiary.weather} onChange={(event) => setNewDiary({ ...newDiary, weather: event.target.value as Weather})} /></div>
